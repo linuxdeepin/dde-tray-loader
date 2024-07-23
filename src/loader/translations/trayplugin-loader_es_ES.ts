6c7117b27972a1e0287aac706e67ce6688c008af
swq <shenwenqi@uniontech.com>
2024-07-23 13:32:57 +0800

    fix: Added trayplugin-loader translation
    Issue: https://github.com/linuxdeepin/developer-center/issues/9754
    Issue: https://github.com/linuxdeepin/developer-center/issues/9685

---
+++ src/loader/translations/trayplugin-loader_es_ES.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>QuickPluginItem</name>
+    <message>
+        <location filename="../quickpluginitem.cpp" line="120"/>
+        <source>Remove from dock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../quickpluginitem.cpp" line="120"/>
+        <source>Pin to dock</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
