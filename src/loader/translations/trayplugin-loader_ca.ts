33a4d950eccfa03ddc20f93ee0a763546a1676b5
zhaoyingzhen <zhaoyingzhen@uniontech.com>
2024-08-27 15:35:34 +0800

    chore: update translation
    as title
    
    Log: as title

--- src/loader/translations/trayplugin-loader_ca.ts
+++ src/loader/translations/trayplugin-loader_ca.ts
@@ -1,13 +1,23 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="ca">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>PluginItem</name>
+    <message>
+        <location filename="../pluginitem.cpp" line="89"/>
+        <source>Remove from dock</source>
+        <translation>Elimina de l&apos;acoblador</translation>
+    </message>
+</context>
 <context>
     <name>QuickPluginItem</name>
     <message>
-        <location filename="../quickpluginitem.cpp" line="120"/>
+        <location filename="../quickpluginitem.cpp" line="160"/>
         <source>Remove from dock</source>
         <translation>Elimina de l&apos;acoblador</translation>
     </message>
     <message>
-        <location filename="../quickpluginitem.cpp" line="120"/>
+        <location filename="../quickpluginitem.cpp" line="160"/>
         <source>Pin to dock</source>
         <translation>Fixa a l&apos;acoblador</translation>
     </message>
