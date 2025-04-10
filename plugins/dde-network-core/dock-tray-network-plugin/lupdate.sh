6e655c1232fc1afd9930fb5c63d0ef951263cf5b
caixiangrong <caixiangrong@uniontech.com>
2025-04-10 09:16:50 +0800

    feat: Remove network module
    Remove network module
    
    pms: TASK-361719

--- plugins/dde-network-core/dock-tray-network-plugin/lupdate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -d "translations/" ];then
-  mkdir translations
-fi
-cd ./translations
-rm -f dock-network-plugin.ts
-lupdate ../ ../../net-view/ -ts -no-ui-lines -locations none -no-obsolete dock-network-plugin.ts
-cd ../
