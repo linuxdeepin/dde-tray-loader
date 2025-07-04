fe84edc6712c984bb7626124e24eb3d2a55386d8
wjyrich <wujiangyu@uniontech.com>
2025-07-04 17:23:10 +0800

    feat: add Traditional Chinese translations for tray plugin
    1. Added new translation files for Traditional Chinese (zh_HK and zh_TW)
    2. Translated "Remove from dock" and "Pin to dock" strings
    3. Different translations provided for PluginItem and QuickPluginItem
    contexts
    4. Supports Hong Kong and Taiwan Chinese variants with appropriate
    terminology
    
    feat: 添加托盘插件的繁体中文翻译
    
    1. 新增繁体中文(香港和台湾)翻译文件
    2. 翻译了"Remove from dock"和"Pin to dock"字符串
    3. 为PluginItem和QuickPluginItem上下文提供不同的翻译
    4. 支持香港和台湾的中文变体并使用适当的术语

---
+++ src/loader/translations/trayplugin-loader_zh_TW.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>PluginItem</name>
+    <message>
+        <location filename="../pluginitem.cpp" line="89"/>
+        <source>Remove from dock</source>
+        <translation>拿掉駐留</translation>
+    </message>
+</context>
+<context>
+    <name>QuickPluginItem</name>
+    <message>
+        <location filename="../quickpluginitem.cpp" line="160"/>
+        <source>Remove from dock</source>
+        <translation>從任務列移除</translation>
+    </message>
+    <message>
+        <location filename="../quickpluginitem.cpp" line="160"/>
+        <source>Pin to dock</source>
+        <translation>固定到任務列</translation>
+    </message>
+</context>
+</TS>
