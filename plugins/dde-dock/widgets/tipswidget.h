22e14d5c9a04b27a85f7ed954e8721a3499f0c60
Wang Zichong <wangzichong@deepin.org>
2025-11-21 16:34:24 +0800

    fix: tooltip size might not updated when font size changed
    目前TipsWidget控件的尺寸会在字体变化时通过调用setText()更新,但当文字
    内容无变化时会跳过后续逻辑,导致尺寸未变化.
    
    此提交通过重新实现 (minimum)sizeHint() 来提供控件期望的尺寸大小,并
    确保尺寸能够及时更新.
    
    PMS: BUG-332523
    Log:

--- plugins/dde-dock/widgets/tipswidget.h
+++ plugins/dde-dock/widgets/tipswidget.h
@@ -22,10 +22,12 @@ public:
     const QStringList &textList() { return  m_textList; }
     void setText(const QString &text);
     void setTextList(const QStringList &textList);
+    QSize sizeHint() const override;
 
 protected:
     void paintEvent(QPaintEvent *event) override;
     bool event(QEvent *event) override;
+    QSize minimumSizeHint() const override;
 
 private:
     QString m_text;
